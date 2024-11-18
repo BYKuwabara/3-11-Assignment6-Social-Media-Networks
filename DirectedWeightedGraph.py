67a9c97afdb090e392a12af3ae85387335524148
jarrenseson <jarrenys@hawaii.edu>
2024-11-17 16:44:35 -1000

    updated documentation

--- DirectedWeightedGraph.py
+++ DirectedWeightedGraph.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
 
-## This class will be used to represent connections between users
+# This class will be used to represent connections between users
 
 class DirectedWeightedGraph:
     def __init__(self):
         self.graph = defaultdict(dict)
 
-    ## Weight will be a string with a value of 'follows', 'friends', 'coworker', 'blocked', or 'has read posts by'.
+    # Weight will be a string with a value of 'follows', 'friends', 'coworker', 'blocked', or 'has read posts by'.
     def addEdge(self, u, v, weight):
         self.graph[u][v] = weight
     
@@ -16,4 +16,4 @@ class DirectedWeightedGraph:
             for v in self.graph:
                 edges.append((u,v,self.graph[u][v]))
         return edges
-    ## O(u * v) or O(n^2)
+    # O(u * v) or O(n^2)
