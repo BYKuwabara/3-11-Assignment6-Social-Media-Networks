e0c312f8659ba7f55e2e0a090de8ec84c580051f
jarrenseson <jarrenys@hawaii.edu>
2024-11-17 16:30:26 -1000

    created post class

---
+++ Post.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, Content, Comments, Views, CreationDateAndTime):
+        self.content = Content
+        self.comments = Comments
+        self.views = Views
+        self.creationDateAndTime = CreationDateAndTime
