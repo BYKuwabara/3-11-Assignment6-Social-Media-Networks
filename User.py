1499ca50da3e15fdb06779a43f30a4e8a403b881
jarrenseson <jarrenys@hawaii.edu>
2024-11-17 16:47:07 -1000

    updated documentation

--- User.py
+++ User.py
@@ -1,5 +1,7 @@
 class User:
-    def __init__(self, UserName, Connections, Posts):
+    def __init__(self, UserName, Connections, Posts, PostedComments, ViewedPosts):
         self.userName = UserName
         self.connections = Connections # Will be represented using DirectedWeightedGraph class
-        self.posts = Posts # Will be represented using Posts class
+        self.posts = Posts # Will be represented by a list of Posts
+        self.postedComments = PostedComments # Will be represented by a list of strings
+        self.viewedPosts = ViewedPosts # Will be represented by a list of Posts
