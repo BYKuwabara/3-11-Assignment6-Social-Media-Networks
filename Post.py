24791b1d1b7fc4e2ebcc03881703fe74781fb42b
jarrenseson <jarrenys@hawaii.edu>
2024-11-17 16:35:59 -1000

    added documentation to attributes of post

--- Post.py
+++ Post.py
@@ -1,6 +1,6 @@
 class Post:
     def __init__(self, Content, Comments, Views, CreationDateAndTime):
-        self.content = Content
-        self.comments = Comments
-        self.views = Views
-        self.creationDateAndTime = CreationDateAndTime
+        self.content = Content ## String
+        self.comments = Comments ## Dictionary with comment as key and user as value
+        self.views = Views ## List of usernames that viewed a post
+        self.creationDateAndTime = CreationDateAndTime ## Creation date and time with the format of: month/day/year:time EX: 08/13/24:1340 => August 13 2024, 1:40 PM
