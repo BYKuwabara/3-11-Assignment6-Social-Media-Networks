69eceff7457f501b4079d914396654508816f639
jarrenseson <jarrenys@hawaii.edu>
2024-11-19 19:26:53 -1000

    Update SocialMediaDiagram.py

--- SocialMediaDiagram.py
+++ SocialMediaDiagram.py
@@ -14,10 +14,19 @@ def SocialMediaDiagram(listOfUsers):
         # Adds authored posts as nodes and creates edges between associated user
         for authoredPost in listOfUsers.posts:
             G.add_node(authoredPost)
-            G.add_edge(user, authoredPost, 'authored')
+            G.add_edge(user, authoredPost, status='authored')
         
         # Adds viewed posts as nodes and creates edges between associated user
         for viewedPost in listOfUsers.viewedPosts:
-            G.add_edge(user, viewedPost, 'viewed')
+            G.add_node(viewedPost)
+            G.add_edge(user, viewedPost, status='viewed')
         
+    # Gets the weight of the edges for graphing
+    edge_weights = nx.get_edge_attributes(G, 'status')
+
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_weights)
+
+    plt.show()
         
